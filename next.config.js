459d69a0101bf3bfbb6420986d6738aad43122b6
devadh21 <devadh21@gmail.com>
2025-05-06 16:25:27 +0000

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// /** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ["placehold.co","pixabay.com","lh3.googleusercontent.com"],
+      },
+
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    // output:'export',
+    // distDir: 'build',
+}
+
+module.exports = nextConfig
