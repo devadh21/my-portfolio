dab93ce195ae7d7720bf802ede172268d746a974
devadh21 <devadh21@gmail.com>
2025-07-16 11:23:23 +0000

    modify link bee honey store

--- app/data.tsx
+++ app/data.tsx
@@ -242,7 +242,7 @@ export const cardInfoContact = [
 export const cardListsPortfolio =[
   {
       id:1,
-      href: "https://beehoney.devadh.info/",
+      href: "https://beehoney.devadh.com/",
       img: "/img/honeyStore.png",
       title: 'Honey Store',
   },
